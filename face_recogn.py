cb7453fd9fc5957afbd7842b0f2c142f1c0d4afc
phtea <cerejapushmenkov2@gmail.com>
2024-03-11 00:52:10 +0300

    Face recognition done ✅

---
+++ face_recogn.py
@@ -0,0 +1,147 @@
+import os
+import cv2
+import face_recognition
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+class Face:
+    def __init__(self, bounding_box, cropped_face, name, feature_vector):
+        self.bounding_box = bounding_box
+        self.cropped_face = cropped_face
+        self.name = name
+        self.feature_vector = feature_vector
+
+class Task_1:
+    def __init__(self, image_path):
+        self.image_path = image_path
+        # Load the XML files for face, eye, and smile detection
+        self.face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
+        self.eye_cascade = cv2.CascadeClassifier('data/haarcascade_eye.xml')
+        self.smile_cascade = cv2.CascadeClassifier('data/haarcascade_smile.xml')  # Path to your smile cascade XML
+        self.detect_face_eyes_smile()
+        
+    def detect_face_eyes_smile(self):
+
+        image = cv2.imread(self.image_path)
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        faces = self.face_cascade.detectMultiScale(gray_image, 1.3, 5)
+
+        for (x,y,w,h) in faces:
+            cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2) # draw a rectangle
+
+            # Define region of interest (ROI) for eyes within the face
+            roi_gray = gray_image[y:y+h, x:x+w]
+            roi_color = image[y:y+h, x:x+w]
+            
+            eyes = self.eye_cascade.detectMultiScale(roi_gray)
+            for (ex,ey,ew,eh) in eyes:
+                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+            smiles = self.smile_cascade.detectMultiScale(roi_gray, scaleFactor=1.8, minNeighbors=20)
+            for (sx,sy,sw,sh) in smiles:
+                cv2.rectangle(roi_color,(sx,sy),(sx+sw,sy+sh),(0,0,255),2)
+
+        # Display the final image
+        cv2.imshow('Detected faces, eyes, and smiles', image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+class Task_2:
+    def __init__(self, image_path, faces):
+        self.faces = faces
+        self.image_path = image_path
+        self.execute()
+
+    def load_image(self, path):
+        image = cv2.imread(path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        return image
+
+    def show_image(self, image):
+        plt.imshow(image)
+        plt.xticks([])
+        plt.yticks([])
+        plt.show()
+
+    def draw_bounding_box(self, image_test, loc_test):
+        top, right, bottom, left = loc_test
+        line_color = (0, 255, 0)
+        line_thickness = 2
+        cv2.rectangle(image_test, (left, top), (right, bottom), line_color, line_thickness)
+        return image_test
+
+    def draw_name(self, image_test, loc_test, pred_name):
+        top, right, bottom, left = loc_test
+        font_style = cv2.FONT_HERSHEY_SIMPLEX
+        font_scale = 1.5
+        font_color = (0, 0, 255)
+        font_thickness = 3
+        text_size, _ = cv2.getTextSize(pred_name, font_style, font_scale, font_thickness)
+        bg_top_left = (left, top - text_size[1])
+        bg_bottom_right = (left + text_size[0], top)
+        line_color = (0, 255, 0)
+        line_thickness = -1
+        cv2.rectangle(image_test, bg_top_left, bg_bottom_right, line_color, line_thickness)
+        cv2.putText(image_test, pred_name, (left, top), font_style, font_scale, font_color, font_thickness)
+        return image_test
+
+    def detect_faces(self, image_test, threshold=0.6):
+        locs_test = face_recognition.face_locations(image_test, model='hog')
+        vecs_test = face_recognition.face_encodings(image_test, locs_test, num_jitters=1)
+        for loc_test, vec_test in zip(locs_test, vecs_test):
+            distances = []
+            for face in self.faces:
+                distance = face_recognition.face_distance([vec_test], face.feature_vector)
+                distances.append(distance)
+            if np.min(distances) > threshold:
+                pred_name = 'unknown'
+            else:
+                pred_index = np.argmin(distances)
+                pred_name = self.faces[pred_index].name
+            image_test = self.draw_bounding_box(image_test, loc_test)
+            image_test = self.draw_name(image_test, loc_test, pred_name)
+        return image_test
+
+    def execute(self):
+        image_test = self.load_image(self.image_path)
+        result_image = self.detect_faces(image_test)
+        self.show_image(result_image)
+
+def create_database() -> list[object]:
+    
+    def get_filenames() -> list[str]:
+        filenames = os.listdir('templates')
+        filenames = [filename for filename in filenames if not filename.startswith('!')]
+        return filenames
+    filenames = get_filenames()
+
+    def load_image(path):
+        image = cv2.imread(path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        return image
+
+    faces = []
+    for filename in tqdm(filenames, total=len(filenames)):
+        image = load_image(os.path.join('templates', filename))
+        loc = face_recognition.face_locations(image, model='hog')[0]
+        vec = face_recognition.face_encodings(image, [loc], num_jitters=20)[0]
+        top, right, bottom, left = loc
+        cropped_face = image[top:bottom, left:right]
+        face = Face(bounding_box=loc, cropped_face=cropped_face, name=filename.split('.')[0], feature_vector=vec)
+        faces.append(face)
+    return faces
+
+def main():
+    Task_1(image_path="input/task1_test1.png")
+    
+    faces = create_database()
+    
+    Task_2(image_path="input/task2_test4.jpg", faces=faces)
+    Task_2(image_path="input/task2_test3.jpg", faces=faces)
+
+
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import face_recogn as fr
 
 def luhn_checksum(card_number):
     """Calculate the Luhn checksum."""
@@ -21,70 +22,58 @@ def find_valid_digits(card_number):
     return None
 
 def diffie_hellman_protocol(p: int, g: int, bob_private_key: int, alice_private_key: int): 
-    # Вычисление публичных ключей
+
+    def calculate_shared_secret(public_key, private_key, p):
+        return pow(public_key, private_key, p)
+    
+    def caesar_encrypt(message, key):
+        encrypted_message = ""
+        for char in message:
+            if char.isalpha():
+                shift = ord('A') if char.isupper() else ord('a')
+                encrypted_char = chr((ord(char) - shift + key) % 26 + shift)
+                encrypted_message += encrypted_char
+            else:
+                encrypted_message += char
+        return encrypted_message
+    
+    def caesar_decrypt(encrypted_message, key):
+        decrypted_message = ""
+        for char in encrypted_message:
+            if char.isalpha():
+                shift = ord('A') if char.isupper() else ord('a')
+                decrypted_char = chr((ord(char) - shift - key) % 26 + shift)
+                decrypted_message += decrypted_char
+            else:
+                decrypted_message += char
+        return decrypted_message
+
     bob_public_key = pow(g, bob_private_key, p)
     alice_public_key = pow(g, alice_private_key, p)
 
-    # Вычисление общего секретного ключа
     bob_shared_secret = calculate_shared_secret(alice_public_key, bob_private_key, p)
     alice_shared_secret = calculate_shared_secret(bob_public_key, alice_private_key, p)
 
-    # Проверка общего секретного ключа
     assert bob_shared_secret == alice_shared_secret
 
-    # Вывод общего секретного ключа
     print("Общий секретный ключ:", bob_shared_secret)
 
-    # Сообщение для шифрования
     message = "HELLO"
-
-    # Шифрование сообщения на стороне Боба с использованием алгоритма Цезаря
     encrypted_message = caesar_encrypt(message, bob_shared_secret)
-
-    # Вывод зашифрованного сообщения
     print("Зашифрованное сообщение на стороне Боба:", encrypted_message)
 
-    # Дешифровка сообщения на стороне Алисы
     decrypted_message = caesar_decrypt(encrypted_message, alice_shared_secret)
-
-    # Вывод расшифрованного сообщения
     print("Расшифрованное сообщение на стороне Алисы:", decrypted_message)
-
-# Функция для вычисления общего секретного ключа
-def calculate_shared_secret(public_key, private_key, p):
-    return pow(public_key, private_key, p)
-
-# Функция для шифрования сообщения с помощью алгоритма Цезаря
-def caesar_encrypt(message, key):
-    encrypted_message = ""
-    for char in message:
-        if char.isalpha():
-            shift = ord('A') if char.isupper() else ord('a')
-            encrypted_char = chr((ord(char) - shift + key) % 26 + shift)
-            encrypted_message += encrypted_char
-        else:
-            encrypted_message += char
-    return encrypted_message
-
-# Функция для дешифрования сообщения с помощью алгоритма Цезаря
-def caesar_decrypt(encrypted_message, key):
-    decrypted_message = ""
-    for char in encrypted_message:
-        if char.isalpha():
-            shift = ord('A') if char.isupper() else ord('a')
-            decrypted_char = chr((ord(char) - shift - key) % 26 + shift)
-            decrypted_message += decrypted_char
-        else:
-            decrypted_message += char
     return decrypted_message
 
+
+def main():
+    # diffie_hellman_protocol(13, 6, 5, 4)
+    # fr.Task_1(image_path="input/task1_test1.png")
+    faces = fr.create_database()
+    fr.Task_2(image_path="input/task2_test4.jpg", faces=faces)
+    fr.Task_2(image_path="input/task2_test2.jpg", faces=faces)
+    fr.Task_2(image_path="input/task2_test3.jpg", faces=faces)
+
 if __name__ == "__main__":
-    # Параметры протокола
-    p = 13
-    g = 6
-    bob_private_key = 5
-    alice_private_key = 4
-
-    diffie_hellman_protocol(p, g, bob_private_key, alice_private_key)
-
-
+    main()
